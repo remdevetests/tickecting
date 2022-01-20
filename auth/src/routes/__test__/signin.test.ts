96a799657f5307088f00f197598ea96a494deee8
Remember <remdevetest@gmail.com>
2022-01-20 10:54:26 +0200

    Testing

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -30,13 +30,13 @@ it('fails when an incorrect password is supplied', async () => {
 });
 
 it('responds with a cookie when given valid credentials', async () => {
-  // await request(app)
-  //   .post('/api/users/signup')
-  //   .send({
-  //     email: 'test@test.com',
-  //     password: 'password'
-  //   })
-  //   .expect(201);
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
 
   const response = await request(app)
     .post('/api/users/signin')
