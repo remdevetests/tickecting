0f4300223a9c49d68fb2ae1e0450c1f0002d4a21
Remember <remdevetest@gmail.com>
2022-01-20 07:57:42 +0200

    Initial commit

---
+++ client/pages/index.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+const LandingPage = ({ currentUser, data }) => {
+  console.log("currentUser : " + currentUser);
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
+
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return { name: "Remember",  surname: "Mabunda" };
+};
+
+export default LandingPage;
